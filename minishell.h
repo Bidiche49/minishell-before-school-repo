f1652aa33b14dcbfaead255810d6c1a8077bc812
audrye <augustin.drye@gmail.com>
2023-07-20 18:37:11 +0200

    mise en place de la structure

---
+++ minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: augustindrye <augustindrye@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 18:31:41 by augustindry       #+#    #+#             */
+/*   Updated: 2023/07/20 18:34:03 by augustindry      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct Commande 
+{
+	char *cmd;
+	char *absolut_path;
+	char **options;
+	int nb_options;
+	char *redir;
+	char **args;
+	int nb_args;
+	char *operateur;
+	struct Commande *next;
+}
+
+#endif
