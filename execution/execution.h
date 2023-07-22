11c7a1e024fe56d8e48bcd50749e25737a84d633
audrye <augustin.drye@gmail.com>
2023-07-22 16:17:59 +0200

    execution d'une seul comande

--- execution/execution.h
+++ execution/execution.h
@@ -6,7 +6,7 @@
 /*   By: augustindrye <augustindrye@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 19:15:54 by augustindry       #+#    #+#             */
-/*   Updated: 2023/07/22 10:52:43 by augustindry      ###   ########.fr       */
+/*   Updated: 2023/07/22 15:58:03 by augustindry      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 # include "../minishell.h"
 # include <stdlib.h>
 
-
+int	general_exe(t_token *token);
 int	ft_execute_cmd(t_token *token);
+int	execute_fork_cmd(t_token *token);
 
 char	**ft_split(char const *s, char c);
 static char	**ft_empty_malloc(void);
@@ -26,5 +27,7 @@ static int	ft_size(char const *s, char c, int i);
 static int	ft_count(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int ft_strlen(const char *str);
+
+int	ft_lstsize(t_token *lst);
 
 #endif
